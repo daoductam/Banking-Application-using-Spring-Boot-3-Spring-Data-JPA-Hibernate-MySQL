97cfaf84a5e0121e09c57c86c9abe2dd238f2454
daoductam174 <tamdao1742005@gmail.com>
2025-03-20 11:01:54 +0700

    change dto class -> dto record

--- src/main/java/com/tamdao/banking/mapper/AccountMapper.java
+++ src/main/java/com/tamdao/banking/mapper/AccountMapper.java
@@ -6,9 +6,9 @@ import dto.AccountDto;
 public class AccountMapper {
     public static Account mapToAccount(AccountDto accountDto) {
         Account account = new Account(
-                accountDto.getId(),
-                accountDto.getAccountHolderName(),
-                accountDto.getBalance()
+                accountDto.id(),
+                accountDto.accountHolderName(),
+                accountDto.balance()
         );
         return account;
     }
--- src/main/java/dto/AccountDto.java
+++ src/main/java/dto/AccountDto.java
@@ -1,15 +1,21 @@
 package dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//
+//@Data
+//@AllArgsConstructor
+//public class AccountDto {
+//    private Long id;
+//    private String accountHolderName;
+//    private double balance;
+//
+//
+//}
 
-@Data
-@AllArgsConstructor
-public class AccountDto {
-    private Long id;
-    private String accountHolderName;
-    private double balance;
-
+public record AccountDto(Long id,
+                         String accountHolderName,
+                         double balance) {
 
 }
 
